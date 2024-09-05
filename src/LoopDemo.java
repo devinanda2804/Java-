fab94d4bb28fedaa6fd40331dfb203c15ff26ebd
devinanda2804 <84197607+devinanda2804@users.noreply.github.com>
2024-09-05 18:35:26 +0530

    Added enhanced for loop

---
+++ src/LoopDemo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+class LoopDemo{
+    public static void main(String args[]){
+        int newArray[]={1,2,3,4};
+        for(int num:newArray){
+            System.out.println(num);
+        }
+    }
+
+}
