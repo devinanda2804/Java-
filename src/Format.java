c8b118676bff985b7cfa778010d78f7abbf29869
devinanda2804 <84197607+devinanda2804@users.noreply.github.com>
2024-09-05 16:28:43 +0530

    Used printf

--- src/Format.java
+++ src/Format.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 class Format{
     public static void main(String[] args){
         int a=100000;
@@ -6,5 +7,9 @@ class Format{
         double b=1.6123549;
         System.out.printf("%f\n",b);
         System.out.printf("%5.2f\n",b);
+        Date time=new Date();
+        System.out.printf("%tT\n ",time);
+        System.out.printf("Hours:%tH Minutes:%tM Seconds:%tS\n",time,time,time);
+
     }
 }
