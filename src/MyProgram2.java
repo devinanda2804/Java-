af3287376708deca7c4b85cc92b2d60e4768bd7f
devinanda2804 <84197607+devinanda2804@users.noreply.github.com>
2024-09-05 18:25:39 +0530

    Decision making

---
+++ src/MyProgram2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+class MyProgram2{
+    public static void main(String[] args){
+        int num=1;
+        while(num<=5){
+            System.out.println(num);
+            num++;
+        }
+    }
+
+}
