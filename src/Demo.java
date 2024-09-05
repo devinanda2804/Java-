5328d7710e2cb85227881692ca9d37ce515c8945
devinanda2804 <84197607+devinanda2804@users.noreply.github.com>
2024-09-05 12:16:01 +0530

    Added Demo java file

---
+++ src/Demo.java
@@ -0,0 +1,9 @@
+class Demo{
+    public static void main(String[] args){
+        int a=10;
+        int b=5;
+        System.out.println("a+b=" +(a+b));
+        System.out.println("Post increment:" +(a++));
+        System.out.println("Pre increment:"+(++a));
+    }
+}
