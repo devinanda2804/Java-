20d292de346358e8956109e399543bd88caa6479
devinanda2804 <84197607+devinanda2804@users.noreply.github.com>
2024-09-05 16:11:18 +0530

    Added print

---
+++ src/Format.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class Format{
+    public static void main(String[] args){
+        int a=100000;
+        System.out.printf("%d\n",a);
+        double b=1.6123549;
+        System.out.printf("%f\n",b);
+        System.out.printf("%5.2f\n",b);
+    }
+}
