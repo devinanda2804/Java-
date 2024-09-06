2f2dc17d14324603acd65d36da03956f3d940a0a
devinanda2804 <84197607+devinanda2804@users.noreply.github.com>
2024-09-06 16:05:55 +0530

    Added StringBuilder methods

---
+++ src/ST1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+class ST1{
+    public static void main(String[] args){
+        StringBuilder str=new StringBuilder();
+        str.append("Good");
+        str.append(" ");
+        str.append("Luck");
+        System.out.println(str.toString());
+        str.insert(9," People");
+        System.out.println(str.toString());
+
+    }
+}
+
