18258fa9320ad6f5ea60cb70c8ca65534cffa89f
devinanda2804 <84197607+devinanda2804@users.noreply.github.com>
2024-09-10 12:32:50 +0530

    Added

--- src/EA.java
+++ src/EA.java
@@ -1,21 +1,36 @@
+//Program with get() and set() methods demonstrating encapsulation for private
+
 class Person1{
     private String name;
     private int age;
+    /*
+            * @param name the name of the person
+     */
     public void set(String name){
         this.name=name;
     }
+    /*
+     * @param age the age of the person
+     */
     public void setAge(int age){
         this.age=age;
     }
+    /* @return the name of the person
+     */
     public String get(){
         return name;
     }
+    /* @return the age of the person
+     */
     public int getAge(){
         return age;
     }
 
 }
 class EA{
+    /*
+            * @param args command-line arguments (not used)
+     */
     public static void main(String[] args){
         Person1 obj=new Person1();
         obj.set("Dev");
