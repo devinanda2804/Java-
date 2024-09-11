3224a4c298acac6d87eab1a5bb155cb69fe65ab1
devinanda2804 <84197607+devinanda2804@users.noreply.github.com>
2024-09-11 17:28:47 +0530

    Added

---
+++ src/Test19.java
@@ -0,0 +1,17 @@
+
+// Java program to delete a file
+import java.io.*;
+
+public class Test19 {
+    public static void main(String[] args)
+    {
+        File f = new File("\"C:\\Users\\devin\\file.docx");
+
+        if (f.delete()) {
+            System.out.println("File deleted successfully");
+        }
+        else {
+            System.out.println("Failed to delete the file");
+        }
+    }
+}
