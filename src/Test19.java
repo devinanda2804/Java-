003865787c66ff2118cea3d5b914c056cc7e68bf
devinanda2804 <84197607+devinanda2804@users.noreply.github.com>
2024-09-11 17:50:31 +0530

    Update Test19.java

--- src/Test19.java
+++ src/Test19.java
@@ -6,7 +6,7 @@ import java.io.*;
 public class Test19 {
     public static void main(String[] args)
     {
-        File f = new File("\"C:\\Users\\devin\\file.docx");
+        File f = new File("C:\\Users\\devin\\file.docx");
 
         if (f.delete()) {
             System.out.println("File deleted successfully");
