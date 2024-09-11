259ffe731735499e2ad669df1163ef890dd6ca51
devinanda2804 <84197607+devinanda2804@users.noreply.github.com>
2024-09-11 17:50:46 +0530

    Update Test20.java

--- src/Test20.java
+++ src/Test20.java
@@ -6,7 +6,7 @@ import java.nio.file.*;
 class Test20{
     public static void main(String[] args){
         try {
-            Files.deleteIfExists(Paths.get("\"C:\\Users\\devin\\file.docx"));
+            Files.deleteIfExists(Paths.get("C:\\Users\\devin\\file.docx"));
         }
         catch(IOException e){
             System.out.println("No such file");
